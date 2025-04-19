f556cc4beab491365239aa12569119940270d6a7
Rimahmtr1 <remahmattar@gmail.com>
2025-04-19 14:54:43 +0300

    styling

--- signup.js
+++ signup.js
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCJsJsuMx1LT6SXZcCqdHa5wkueqXTTT4Q",
   authDomain: "phone-maintenance-18b38.firebaseapp.com",
@@ -15,10 +19,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
+const db = firebase.firestore();
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('Signup');
     const messageElement = document.getElementById('message');
@@ -35,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = document.getElementById('email').value.trim();
         const password = document.getElementById('password').value.trim();
         const confirmPassword = document.getElementById('c-password').value.trim();
+        
         // Call the validation function
         const validationResult = validateForm(firstName, lastName, phone, email, password, confirmPassword);
 
