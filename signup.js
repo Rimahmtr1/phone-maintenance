c41abb905a92869d0aeea72397ad8f0dd1cae3ec
Rimahmtr1 <remahmattar@gmail.com>
2025-04-19 19:33:57 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
